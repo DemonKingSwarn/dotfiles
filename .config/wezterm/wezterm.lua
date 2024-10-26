e7ea32c75df0eb4f52d68dea8d48fbe567d3d039
DemonKingSwarn <rockingswarn@gmail.com>
2024-10-26 19:24:53 +0530

    put code that worked where the code that didn't used to be
     an automated commit

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,12 @@
 local wezterm = require("wezterm")
 return {
-	default_cursor_style = "SteadyBar",
-	font = wezterm.font("JetBrains Mono Nerd Font"),
-	font_size = 16.0,
-	enable_tab_bar = false,
-	enable_wayland = true,
-	window_background_opacity = 0.9,
-	window_close_confirmation = "NeverPrompt",
-	-- default_prog = { "nu" },
-	default_prog = { "zsh" },
-    color_scheme = 'Dracula',
+  default_cursor_style = "SteadyBar",
+  font = wezterm.font("JetBrains Mono Nerd Font"),
+  font_size = 16.0,
+  enable_tab_bar = false,
+  enable_wayland = true,
+  window_background_opacity = 0.9,
+  window_close_confirmation = "NeverPrompt",
+  default_prog = { "zsh" },
+  color_scheme = "Dracula (Official)",
 }
