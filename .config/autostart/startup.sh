27d7053c342cb8299466dfaf51aa84308af7ca31
DemonKingSwarn <rockingswarn@gmail.com>
2025-05-09 20:41:48 +0530

    less french words
     an automated commit

--- .config/autostart/startup.sh
+++ .config/autostart/startup.sh
@@ -1,16 +1,38 @@
 #!/bin/sh
 
+pkill -f /usr/libexec/polkit-gnome-authentication-agent-1
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
 /usr/lib/xdg-desktop-portal && /usr/lib/xdg-desktop-portal-hyprland &
+
+pkill -f swaync
 swaync &
+
+pkill -f waybar
 waybar &
+
+
 #flameshot -a &
+
+pkill -f udiskie
 udiskie &
+
+pkill -f xwayland-satellite
 xwayland-satellite &
+
+pkill -f swww-daemon
 swww-daemon &
+
+pkill -f nm-applet
 nm-applet &
+
+pkill -f copyq
 copyq --start-server &
+
+pkill -f blueman-applet
 blueman-applet &
+
+
 #hyprwatch -d &> /dev/null &
 #foot --server &
 
